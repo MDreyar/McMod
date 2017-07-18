335f07565b5c05ca0b09efa60fcbf58a1150026c
MDreyar <stoeremitch@hotmail.com>
2017-07-18 13:05:16 +0200

    add smelting recipe for test ingot

--- src/main/java/m_dreyar/dmrt/init/ModRecipies.java
+++ src/main/java/m_dreyar/dmrt/init/ModRecipies.java
@@ -1,8 +1,11 @@
 package m_dreyar.dmrt.init;
 
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 public class ModRecipies {
 
 	public static void register(){
-		
+		GameRegistry.addSmelting(ModBlocks.testBlock, new ItemStack(ModItems.testIngot, 1), 50f);
 	}
 }
