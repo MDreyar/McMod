ded9d8eec0a4a7135f856a31509ccef2e36648d9
MDreyar <stoeremitch@hotmail.com>
2017-08-10 19:49:48 +0200

    add particle effect to teleporter and tooltip to recall device

--- src/main/java/m_dreyar/dmrt/items/ItemRecall.java
+++ src/main/java/m_dreyar/dmrt/items/ItemRecall.java
@@ -1,8 +1,11 @@
 package m_dreyar.dmrt.items;
 
+import java.util.List;
+
 import m_dreyar.dmrt.Main;
 import m_dreyar.dmrt.Reference;
 import m_dreyar.dmrt.tileentity.TileEntityTeleporter;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -24,10 +27,7 @@ public class ItemRecall extends Item {
 
 	@Override
 	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-		if (worldIn.isRemote) {
-			return EnumActionResult.PASS;
-		}
-
+		// Check for NBT tag, if none found, create one
 		NBTTagCompound nbt;
 		if (player.getHeldItem(hand).hasTagCompound()) {
 			nbt = player.getHeldItem(hand).getTagCompound();
@@ -35,33 +35,20 @@ public class ItemRecall extends Item {
 			nbt = new NBTTagCompound();
 		}
 
-		if (nbt.hasKey("Loc")) {
-			int[] loc = nbt.getIntArray("Loc");
-			BlockPos position = new BlockPos(loc[0], loc[1], loc[2]);
-			if (worldIn.getTileEntity(position) instanceof TileEntityTeleporter) {
-				TileEntityTeleporter tile = (TileEntityTeleporter) worldIn.getTileEntity(position);
-				return tile.attemptTeleport(player);
-			} else {
-				return EnumActionResult.PASS;
-			}
-		} else {
+		if (!nbt.hasKey("Loc")) {
 			if (worldIn.getTileEntity(pos) instanceof TileEntityTeleporter) {
 				int[] loc = { pos.getX(), pos.getY(), pos.getZ() };
 				nbt.setIntArray("Loc", loc);
 				player.getHeldItem(hand).setTagCompound(nbt);
 				return EnumActionResult.SUCCESS;
-			} else {
-				return EnumActionResult.PASS;
 			}
 		}
+		return EnumActionResult.PASS;
 	}
 
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
-		if (worldIn.isRemote) {
-			return new ActionResult<ItemStack>(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
-		}
-
+		// Check for NBT tag
 		NBTTagCompound nbt;
 		if (playerIn.getHeldItem(handIn).hasTagCompound()) {
 			nbt = playerIn.getHeldItem(handIn).getTagCompound();
@@ -81,6 +68,22 @@ public class ItemRecall extends Item {
 			}
 		} else {
 			return new ActionResult<ItemStack>(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
+		}
+	}
+
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		// Check for NBT tag, if none found,
+		NBTTagCompound nbt;
+		if (stack.hasTagCompound()) {
+			nbt = stack.getTagCompound();
+		} else {
+			return;
+		}
+
+		if (nbt.hasKey("Loc")) {
+			int[] loc = nbt.getIntArray("Loc");
+			tooltip.add("Linked to teleporter at X:" + loc[0] + " Y:" + loc[1] + " Z:" + loc[2]);
 		}
 	}
 
--- src/main/java/m_dreyar/dmrt/tileentity/TileEntityTeleporter.java
+++ src/main/java/m_dreyar/dmrt/tileentity/TileEntityTeleporter.java
@@ -1,37 +1,52 @@
 package m_dreyar.dmrt.tileentity;
 
+import java.util.Random;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 
-public class TileEntityTeleporter extends TileEntity{
+public class TileEntityTeleporter extends TileEntity {
 
-	public boolean canTeleportHere(){
+	private boolean canTeleportHere() {
 		BlockPos position = getPos();
-		if(world.getBlockState(position.up()).getBlock().equals(Blocks.AIR)){
-			if(world.getBlockState(position.up(2)).getBlock().equals(Blocks.AIR)){
+		if (world.getBlockState(position.up()).getBlock().equals(Blocks.AIR)) {
+			if (world.getBlockState(position.up(2)).getBlock().equals(Blocks.AIR)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public void teleportHere(EntityPlayer player){
-		BlockPos position = getPos();
-		player.setPositionAndUpdate(position.getX() + 0.5, position.up().getY(), position.getZ() + 0.5);
-	}
-	
-	public EnumActionResult attemptTeleport(EntityPlayer player){
-		if(!world.isRemote){
-			if(canTeleportHere()){
-				teleportHere(player);
-				return EnumActionResult.SUCCESS;
-			}else{
-				return EnumActionResult.FAIL;
-			}
+
+	private void teleportHere(EntityPlayer player) {
+		if(world.isRemote){
+			spawnParticle(500);
+		}else{
+			BlockPos position = getPos();
+			player.setPositionAndUpdate(position.getX() + 0.5, position.up().getY(), position.getZ() + 0.5);
 		}
-		return EnumActionResult.PASS;
+	}
+
+	private void spawnParticle(int amount) {
+		for (int i = 0; i < amount; i++) {
+			Random random = world.rand;
+			double posX = getPos().getX() + random.nextDouble();
+			double posY = getPos().getY() + random.nextDouble();
+			double posZ = getPos().getZ() + random.nextDouble();
+			double speedY = random.nextDouble();
+			world.spawnParticle(EnumParticleTypes.PORTAL, posX, posY, posZ, 0, speedY, 0, new int[0]);
+		}
+	}
+
+	public EnumActionResult attemptTeleport(EntityPlayer player) {
+		if (canTeleportHere()) {
+			teleportHere(player);
+			return EnumActionResult.SUCCESS;
+		} else {
+			return EnumActionResult.FAIL;
+		}
 	}
 }
